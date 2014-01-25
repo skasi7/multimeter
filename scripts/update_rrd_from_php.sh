df0740ad7c1471bce767a8ea3a4f3a7345b5aa04
skasi7 <skasi.7@gmail.com>
2014-01-25 13:36:39 +0100

    Finished update code.

--- scripts/update_rrd_from_php.sh
+++ scripts/update_rrd_from_php.sh
@@ -3,27 +3,27 @@
 rrd_file=$1
 if [ -z $rrd_file ]
 then
-  echo "You must provide a RRD filename!"
+  echo "You must provide a RRD filename!" >&2
   exit 1
 fi
 if [ ! -f $rrd_file ]
 then
-  echo "RRD filename $rrd_file not found!"
+  echo "RRD filename '$rrd_file' not found!" >&2
   exit 1
 fi
 
 template=$2
 if [ -z $template ]
 then
-  echo "You must provide a RRD template!"
+  echo "You must provide a RRD template!" >&2
   exit 1
 fi
 data=$3
 if [ -z $data ]
 then
-  echo "You must provide a RRD data!"
+  echo "You must provide a RRD data!" >&2
   exit 1
 fi
-rrdtool update -t $template $rrd_file N:$data
-return $?
+
+rrdtool update "$rrd_file" -t "$template" "N:$data"
 
--- update.php
+++ update.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="">
+    <meta http-equiv="refresh" content="600">
 
     <title>Multimeter</title>
   </head>
